efecf7c0cd9db8fdaa207f990f87d5798541f385
Imron Hajiahmad <imronha@gmail.com>
2020-08-09 11:14:12 -0700

    implement strStr() completed

--- ImplementstrStr().js
+++ ImplementstrStr().js
@@ -19,3 +19,22 @@
 // Constraints:
 
 // haystack and needle consist only of lowercase English characters.
+
+// /**
+//  * @param {string} haystack
+//  * @param {string} needle
+//  * @return {number}
+//  */
+var strStr = function (haystack, needle) {
+  if (!needle) {
+    return 0;
+  }
+  let i = 0;
+  while (
+    haystack.substring(i, needle.length + i) !== needle &&
+    i <= haystack.length
+  ) {
+    i++;
+  }
+  return i > haystack.length ? -1 : i;
+};
