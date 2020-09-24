7c838b39e862fbdffb7527d3efda80756f2cab00
Imron Hajiahmad <imronha@gmail.com>
2020-09-24 14:36:30 -0700

    bubble sort python completed

--- Sorting_BubbleSort.py
+++ Sorting_BubbleSort.py
@@ -1,3 +1,29 @@
+# Bubble Sort
+# Sorting algorithm where you compare two values, switch them if needed, and then continue iterating through the array until the array is sorted
+
+# Clarification Questions:
+# -- Inputs and outputs?
+# -- Expected size of input?
+# -- Constraints? Time and space?
+
+# (I) Input: An array
+# (O) Output: Sorted Array
+# (C) Constraints: Optimize
+# (E) Edge Cases: Empty array, special chars, non integer array
+
+# Pseudocode:
+# -- Get the length of the array
+# -- Create variable to set sorted as true/False
+# -- Iterate through the array only if the array is still not sorted (use while loop)
+# -- Create for loop to iterate through the array
+# -- Each iteration, check to see if current num is greater / smaller than next num
+# -- If current num is greater than next num, swap the numbers
+# -- return sorted array
+
+# Time Complexity: O(n^2)
+# Space Complexity: O(n)
+
+
 def bubbleSort(listInput):
     listLength = len(listInput) - 1
     sorted = False
