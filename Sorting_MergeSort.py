2da0aee186f302fae77beb473c0387a3dab86454
Imron Hajiahmad <imronha@gmail.com>
2020-10-02 23:58:01 -0700

    merge sort completed

--- Sorting_MergeSort.py
+++ Sorting_MergeSort.py
@@ -7,4 +7,31 @@ def mergeSort(A):
         mergeSort(left)
         mergeSort(right)
 
-        i = 0, j = 0, k = 0
+        # Merge the arrays
+        i = 0
+        j = 0
+        k = 0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                A[k] = left[i]
+                i += 1
+            else:
+                A[k] = right[j]
+                j += 1
+            k = k + 1
+
+        while i < len(left):
+            A[k] = left[i]
+            i = i + 1
+            k = k + 1
+
+        while j < len(right):
+            A[k] = right[j]
+            j = j + 1
+            k = k + 1
+
+
+A = [2, 7, 89, 4, 3, 564, 7, 80]
+print('Original Array', A)
+mergeSort(A)
+print('Sorted Array', A)
