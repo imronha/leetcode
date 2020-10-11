0fcf63bc564bfa51c0b5d6be0c2e8ab229cfc5b6
Imron Hajiahmad <imronha@gmail.com>
2020-10-11 10:09:35 -0700

    1351 count neg num in sorted matrix optimized (binary search method)

--- 1351_CountNegativeNumbersInSortedMatrix.js
+++ 1351_CountNegativeNumbersInSortedMatrix.js
@@ -27,15 +27,54 @@
 // 1 <= m, n <= 100
 // -100 <= grid[i][j] <= 100
 
+// Brute force solution
+// I: m x n matrix
+// O: integer
+// C: m == grid.length, n = grid[i].length
+// E:
+// Time Complexity: O(n)^2 - double for loop
+// Space Complexity: O(n)
+// var countNegatives = function (grid) {
+//   let negativeCount = 0;
+//   for (let i = 0; i < grid.length; i++) {
+//     for (let j = 0; j < grid[i].length; j++) {
+//       if (grid[i][j] < 0) {
+//         negativeCount++;
+//       }
+//     }
+//   }
+
+//   return negativeCount;
+// };
+
+// Optimized solution with binary search
+// I: m x n matrix
+// O: integer
+// C: m == grid.length, n = grid[i].length
+// E:
 var countNegatives = function (grid) {
   let negativeCount = 0;
-  for (let i = 0; i < grid.length; i++) {
-    for (let j = 0; j < grid[i].length; j++) {
-      if (grid[i][j] < 0) {
-        negativeCount++;
-      }
+  for (let row of grid) {
+    const index = binarySearch(row);
+    if (index > -1) {
+      negativeCount += row.length - index;
     }
   }
 
   return negativeCount;
+};
+var binarySearch = function (nums) {
+  let startpoint = 0;
+  let endpoint = nums.length - 1;
+  let result = -1;
+  while (startpoint <= endpoint) {
+    let midpoint = Math.floor((startpoint + endpoint) / 2);
+    if (nums[midpoint] < 0) {
+      result = midpoint;
+      endpoint = midpoint - 1;
+    } else {
+      startpoint = midpoint + 1;
+    }
+  }
+  return result;
 };
