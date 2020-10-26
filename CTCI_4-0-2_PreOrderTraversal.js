088fa838e3ddd541685ce9824292ac5a4fee1c6f
Imron Hajiahmad <imronha@gmail.com>
2020-10-25 19:47:18 -0700

    pre order traversal updated with tests

--- CTCI_4-0-2_PreOrderTraversal.js
+++ CTCI_4-0-2_PreOrderTraversal.js
@@ -27,12 +27,16 @@ Pseudocode
 
 */
 
-function TreeNode(val) {
-  this.val = val;
-  this.left = this.right = null;
+class TreeNode {
+  constructor(data, left = null, right = null) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
+  }
 }
 
 var preorderTraversal = function (root) {
+  // If the root is null, return empty array
   if (root == null) {
     return [];
   }
@@ -42,6 +46,7 @@ var preorderTraversal = function (root) {
 
   stack.push(root);
 
+  // Keep iterating as long as there are still items in stack
   while (stack.length > 0) {
     let current = stack.pop();
     result.push(current.val);
@@ -52,3 +57,35 @@ var preorderTraversal = function (root) {
 
   return result;
 };
+
+/*
+       10
+       / \
+      /   \
+     /     \
+    1       2
+   / \     / \
+  3   4   5   6
+
+*/
+let tree = new TreeNode(10);
+let node1 = new TreeNode(1);
+let node2 = new TreeNode(2);
+
+tree.left = node1;
+tree.right = node2;
+
+let node3 = new TreeNode(3);
+let node4 = new TreeNode(4);
+
+node1.left = node3;
+node1.right = node4;
+
+let node5 = new TreeNode(5);
+let node6 = new TreeNode(6);
+
+node2.left = node5;
+node2.right = node6;
+
+console.log("preOrderTraversal", preorderTraversal(tree));
+// Returns [10, 1, 3, 4, 2, 5, 6]
