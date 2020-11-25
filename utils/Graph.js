b9952c7f5ef7989ea6aa398ac4d36213b2d9c948
Imron Hajiahmad <imronha@gmail.com>
2020-11-25 08:25:17 -0800

    new graph implementation, new graph BFS

--- utils/Graph.js
+++ utils/Graph.js
@@ -63,6 +63,7 @@
 
 // module.exports = Graph;
 
+// -----------------------------------------------------------------
 class Graph {
   constructor(numOfVertices) {
     this.numOfVertices = numOfVertices;
@@ -137,3 +138,50 @@ g.addEdge("C", "F");
 // E -> A D F C
 // F -> E C
 g.printGraph();
+
+// -----------------------------------------------------------------
+
+class Graph {
+  constructor() {
+    this.adjList = {};
+  }
+
+  addVertex(vertex) {
+    // If vertex is not in adjlist, add it as a key and set val to []
+    if (!this.adjList[vertex]) {
+      this.adjList[vertex] = [];
+    }
+  }
+
+  addEdge(start, destination) {
+    if (!this.adjList[start]) {
+      this.addVertex(start);
+    }
+    if (!this.adjList[destination]) {
+      this.addVertex(destination);
+    }
+
+    this.adjList[start].push(destination);
+    this.adjList[destination].push(start);
+  }
+
+  removeEdge(start, destination) {
+    // Set this.adjList[start] to a filtered array that doesnt have destination vertex
+    this.adjList[start] = this.adjList[start].filter(
+      (vertex) => vertex !== destination
+    );
+
+    // Set this.adjList[destination] to a filtered array that doesnt have start vertex
+    this.adjList[destination] = this.adjList[destination].filter(
+      (vertex) => vertex !== start
+    );
+  }
+
+  removeVertex(vertex) {
+    while (this.adjacencyList[vertex]) {
+      const adjacentVertex = this.adjacencyList[vertex].pop();
+      this.removeEdge(vertex, adjacentVertex);
+    }
+    delete this.adjacencyList[vertex];
+  }
+}
