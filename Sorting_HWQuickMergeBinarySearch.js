4b70bb6b2f4d09c559fded7f80d6b4e9fe5540bc
Imron Hajiahmad <imronha@gmail.com>
2021-01-07 11:07:07 -0800

    apprentice hw sorting & searching finished

--- Sorting_HWQuickMergeBinarySearch.js
+++ Sorting_HWQuickMergeBinarySearch.js
@@ -125,11 +125,61 @@ console.log(sortedArray); //prints [2,3,5,6,7,9]
 /*
 =============================================================================
 Merge Sort
+https://medium.com/javascript-in-plain-english/javascript-merge-sort-3205891ac060
 
 1. Visuals:
+https://www.hackerearth.com/practice/algorithms/sorting/merge-sort/visualize/
+https://opendsa-server.cs.vt.edu/embed/mergesortAV
+
 
 2. Plain English Summary:
-
+    -- Recursive solution
+    -- Keep dividing the input array in half until only 1 element is left
+    -- Once there is only 1 element, compare single elements
+    -- Swap if single elements are out of order
+    -- Merge elements using separate merge helper function
+        -- Merge function will loop through inputs and return a merged result array
 3. JS Code:
 
 */
+
+// Merge Sort Implentation (Recursion)
+function mergeSort (unsortedArray) {
+    // No need to sort the array if the array only has one element or empty
+    if (unsortedArray.length <= 1) {
+      return unsortedArray;
+    }
+    // In order to divide the array in half, we need to figure out the middle
+    const middle = Math.floor(unsortedArray.length / 2);
+  
+    // This is where we will be dividing the array into left and right
+    const left = unsortedArray.slice(0, middle);
+    const right = unsortedArray.slice(middle);
+  
+    // Using recursion to combine the left and right
+    return merge(
+      mergeSort(left), mergeSort(right)
+    );
+  }
+
+  // Merge the two arrays: left and right
+function merge (left, right) {
+    let resultArray = [], leftIndex = 0, rightIndex = 0;
+  
+    // We will concatenate values into the resultArray in order
+    while (leftIndex < left.length && rightIndex < right.length) {
+      if (left[leftIndex] < right[rightIndex]) {
+        resultArray.push(left[leftIndex]);
+        leftIndex++; // move left array cursor
+      } else {
+        resultArray.push(right[rightIndex]);
+        rightIndex++; // move right array cursor
+      }
+    }
+  
+    // We need to concat here because there will be one element remaining
+    // from either left OR the right
+    return resultArray
+            .concat(left.slice(leftIndex))
+            .concat(right.slice(rightIndex));
+  }
